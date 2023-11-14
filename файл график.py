1288f62cbdf7d51ece4060af56460f2e044870bd
Liza Vazhesova <i@velizaveta.ru>
2023-11-14 15:26:40 +0300

    Initial commit

---
+++ файл график.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+f1 = open('C:\\Users\hp\Downloads\PZ_Mon_v_radial_1__1.dat', 'r')
+f0 = []
+for line in f1:
+    f = line.split()
+    f0.append(f)
+del f0[0]
+MJD = []
+Vr = []
+for i in range(len(f0)):
+    MJD.append(float(f0[i][0]))
+    Vr.append(float(f0[i][1]))
+plt.scatter(MJD, Vr, color = 'green')
+plt.xlabel('MJD')
+plt.ylabel('Vr')
+plt.title('Vr(MJD)')
+plt.show()
